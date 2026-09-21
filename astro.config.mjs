e37fb8a4906dd789779d38e259daecb2ee2618c9
Satyasy <Satyasy@users.noreply.github.com>
2026-09-21 21:58:31 +0700

    feat: configure custom domain jawarateknikac.com, canonical urls, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jawarateknikac.com',
   integrations: [
     tailwind({
       applyBaseStyles: false,
